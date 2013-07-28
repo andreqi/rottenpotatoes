d1efd0bfebbf502079830fe7bc272c14a873e477
Andre Quispesaravia <drol.005@gmail.com>
2013-07-27 22:28:38 -0500

    second part of edx saas homework

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,15 +5,43 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-def index
+  def index
     @class = Hash.new {|hash, key| hash[key] = ""}
-    if column = params[:sortby]
-      @class[column.to_sym] = "hilite"
-      @movies = Movie.find(:all , :order => column)
-    else 
+    @all_ratings = ['G', 'PG', 'PG-13', 'R'] 
+    if !session.has_key? :index_params
+      index_params = {:ratings => @all_ratings}
+      @rating_value = Hash.new (true)
       @movies = Movie.all
+      session[:index_params] = index_params
+      return
+    end
+    index_params = session[:index_params]
+    column  = index_params[:sortby]
+    ratings = index_params[:ratings]
+
+    if cur_column = params[:sortby]
+      column = cur_column
+    end
+    if cur_ratings = params[:ratings]
+      ratings = cur_ratings.keys
     end
 
+    index_params[:sortby] = column
+    index_params[:ratings] = ratings
+
+    options = Hash.new
+    if column 
+      options[:order] = column 
+      @class[column.to_sym] = "hilite"
+    end
+    if ratings
+      options[:conditions] = ["rating IN (?)", ratings]
+    end
+
+    @rating_value = Hash.new(false)
+    ratings.each {|rating| @rating_value[rating] = true}
+    @movies = Movie.find(:all , options)
+    session[:index_params] = index_params
   end
 
   def new
